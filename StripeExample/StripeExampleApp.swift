8eafc844134ce4076e3c1759818cc52e0da2ab01
Mas'ud <68595786+dev-onimoe@users.noreply.github.com>
2024-06-20 16:04:47 +0100

    Initial Commit

---
+++ StripeExample/StripeExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StripeExampleApp.swift
+//  StripeExample
+//
+//  Created by Masud Onikeku on 20/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct StripeExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
